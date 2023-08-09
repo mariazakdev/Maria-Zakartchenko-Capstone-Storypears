83c21d4d44ab784fbd70db87f6588c58c95457c4
Maria Zak <mzakart@gmail.com>
2023-08-09 19:13:42 -0400

    Added api to each story

--- src/components/StoryReader/StoryReader.jsx
+++ src/components/StoryReader/StoryReader.jsx
@@ -1,36 +1,16 @@
 import NavStoryReader from "../NavStoryReader/NavStoryReader";
 import "./StoryReader.scss";
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
 
-function StoryPage() {
-  const { id } = useParams(); 
-  const [story, setStory] = useState({});
-
-  useEffect(() => {
-    fetchStory();
-  }, []);
-
-  async function fetchStory() {
-    try {
-      const response = await axios.get(
-        `https://shortstories-api.onrender.com/stories/${id}`
-      );
-      setStory(response.data);
-    } catch (error) {
-      console.error("Error fetching story:", error);
-    }
-  }
-
-  return (
+function StoryPage({story}) {
+  
+ return(
     <div className="story-reader">
       <div>
         <NavStoryReader />
       </div>
       <div>
-        <h2>{story.title}</h2>
-        <p>{story.story}</p>
+      <h2>{story.title}</h2>
+      <p>{story.story}</p>
       </div>
       <div>
         <NavStoryReader />
--- src/pages/StoryPage/StoryPage.jsx
+++ src/pages/StoryPage/StoryPage.jsx
@@ -1,13 +1,30 @@
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import StoryReader from '../../components/StoryReader/StoryReader';
+import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-function StoryPage() {
+function StoryPage({match}) {
+  const [story, setStory] = useState(null);
+  const apiURL = process.env.REACT_APP_API_URL;
+  const { id: storyId } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/stories/${storyId}`)
+      .then(response => {
+        setStory(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching story:', error);
+      });
+  }, [apiURL, storyId]);
+
   return (
     <div className="App">
  <Header />
- <StoryReader />
- <Footer />
+ {story ? <StoryReader story={story} /> : <p>Loading...</p>} <Footer />
     </div>
   );
 }
