7ba81d1a161a6b89a7bdc105537f840b7ce5f302
Maria Zak <mzakart@gmail.com>
2023-08-10 09:53:16 -0400

    Added all api's necessary for now

---
+++ src/components/ProfileAdd/ProfileAdd.jsx
@@ -0,0 +1,15 @@
+function ProfileAdd(){
+    return(
+        <div className="create-profile">
+        <section className="create_profile__heading">
+            <h3>Create your writer profile</h3>
+            </section>
+       
+       <section className="create_profile__form-wrapper" >
+      
+       </section>
+       </div>
+    );
+};
+
+export default ProfileAdd;
---
+++ src/pages/ProfileAddPage/ProfileAddPage.jsx
@@ -0,0 +1,17 @@
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import ProfileAdd from '../../components/ProfileAdd/ProfileAdd';
+
+
+function ProfileAddPage() {
+
+    return (
+        <div className="App">
+            <Header />
+            <ProfileAdd />
+            <Footer />
+        </div>
+    );
+}
+
+export default ProfileAddPage;
