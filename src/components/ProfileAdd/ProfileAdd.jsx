5c32c2f1adc420e101ecc8b8af523fa7313ef3e1
Maria Zak <mzakart@gmail.com>
2023-08-10 12:06:18 -0400

    Added some css to form

--- src/components/ProfileAdd/ProfileAdd.jsx
+++ src/components/ProfileAdd/ProfileAdd.jsx
@@ -14,36 +14,40 @@ function ProfileAdd() {
           method="post"
         >
           <label for="username">Username:</label>
-          <input type="text" id="username" name="username" required />
+          <input type="text" id="username" name="username" placeholder="Name for login"required />
 
           <label for="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+          <input type="password" id="password" name="password" placeholder="Create a password" required />
+
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" placeholder="Repeat a password" required />
 
           <label for="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+          <input type="email" id="email" name="email" placeholder="Email for login" required />
 
           <label for="first_name">First Name:</label>
-          <input type="text" id="first_name" name="first_name" required />
+          <input type="text" id="first_name" name="first_name" placeholder="Not visible on site" required />
 
           <label for="last_name">Last Name:</label>
-          <input type="text" id="last_name" name="last_name" required />
+          <input type="text" id="last_name" name="last_name" placeholder="Not visible on site" required />
 
           <label for="pen_first_name">Pen First Name:</label>
           <input
             type="text"
             id="pen_first_name"
             name="pen_first_name"
+            placeholder="Name to be used on site" 
             required
           />
 
           <label for="pen_last_name">Pen Last Name:</label>
-          <input type="text" id="pen_last_name" name="pen_last_name" required />
+          <input type="text" id="pen_last_name" name="pen_last_name" placeholder="Last name optional"  required />
 
           <label for="bio">Bio:</label>
-          <textarea id="bio" name="bio" rows="4" cols="50" />
+          <textarea id="bio" name="bio" placeholder="Tell us something about yourself" rows="4" cols="50" />
 
           <label for="links">Links:</label>
-          <input type="text" id="links" name="links" value="Facebook.com" />
+          <input type="text" id="links" name="links" />
 
           <input type="submit" value="Create Profile" />
         </form>
