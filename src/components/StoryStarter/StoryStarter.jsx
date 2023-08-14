320151e01441d9eb8d7ad2ce05cdb9f4e638f9a4
Maria Zak <mzakart@gmail.com>
2023-08-14 13:39:32 -0400

    Css and toggle added to prompts

--- src/components/StoryStarter/StoryStarter.jsx
+++ src/components/StoryStarter/StoryStarter.jsx
@@ -1,42 +1,52 @@
-import StoryPromptBox from "../StoryPromptBox/StoryPromptBox";
-import StoryEmotionBox from "../StoryEmotionBox/StoryEmotionBox";
+import React, { useState } from "react";
 import "./StoryStarter.scss";
 
-function StoryStarter({prompts, feelings}){
-    return(
+function StoryStarter({ prompts, feelings }) {
+    const [activeContent, setActiveContent] = useState("prompts"); // Initial state is "prompts"
+
+    return (
         <div className="story-starter">
-            <p>StoryStarter block</p>
             <section className="story-starter__heading">
-        <h2>Seed the pears...</h2>
-        <p>Plant your seed on a story prompt you like, wait for someone to join. Together a story pear will grow</p>
-    </section>
-    <section className="story-starter__container">
-                <div className="story-starter__container--story">
-                    <h3>Prompts:</h3>
-                    <ul>
-                        {prompts.map((prompt) => (
-                            <li key={prompt.id}>{prompt.sentence}</li>
-                        ))}
-                    </ul>
+                <h2>Seed the pears...</h2>
+                <p>Plant your seed on a story prompt you like, wait for someone to join. Together a story pear will grow</p>
+            </section>
+            <section className="story-starter__container">
+                <div className="story-starter__container--toggler">
+                    <button onClick={() => setActiveContent("prompts")}>Show Prompts</button>
+                    <button onClick={() => setActiveContent("feelings")}>Show Feelings</button>
                 </div>
-                <div className="story-starter__container--feeling">
-                    <h3>Feelings:</h3>
-                    <ul>
-                        {feelings.map((feeling) => (
-                            <li key={feeling.id}>{feeling.expression}</li>
-                        ))}
-                    </ul>
+                <div className="story-starter__container--content">
+                    {activeContent === "prompts" && (
+                        <div>
+                            <h3>Prompts:</h3>
+                            <ul className="story-item">
+                                {prompts.map((prompt) => (
+                                    <li key={prompt.id}>{prompt.sentence}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                    {activeContent === "feelings" && (
+                        <div>
+                            <h3>Feelings:</h3>
+                            <ul className="feeling-item">
+                                {feelings.map((feeling) => (
+                                    <li key={feeling.id}>{feeling.expression}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </section>
-    <section className="story-starter-form">
+            <section className="story-starter-form">
                 <form action="#" method="post" id="sentenceForm">
                     <label htmlFor="sentence">Enter your story prompt:</label><br />
                     <textarea id="sentence" name="sentence" rows="4" cols="50" required></textarea><br />
                     <input type="submit" value="Add pear seed" />
                 </form>
             </section>
- 
-     </div>
-    )
+        </div>
+    );
 }
-export default StoryStarter; 
+
+export default StoryStarter;
