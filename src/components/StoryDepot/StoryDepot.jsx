3d4c4322752ed1b4a743c2a815d68314fd911c9a
Maria Zak <mzakart@gmail.com>
2023-08-15 15:19:41 -0400

    Added Css to halfstory

--- src/components/StoryDepot/StoryDepot.jsx
+++ src/components/StoryDepot/StoryDepot.jsx
@@ -3,28 +3,27 @@ import "./StoryDepot.scss";
 
 function StoryDepot({ halfStories, handleItemClick }) {
   return (
-    <div className="story-starter">
-
-      <section className="story-starter__heading">
+    <div className="story-depot">
+      <section className="story-depot__heading">
         <h2>Stories waiting for pears...</h2>
         <p>Add your own nourishment so a pear can grow.</p>
       </section>
 
-      <section >
-          <div>
-            <h3>Half Stories:</h3>
-            <ul className="halfstory-item">
+      <section className="story-depot__half-stories">
+        <div>
+          <h3>Half Stories:</h3>
+          <ul>
             {halfStories && halfStories.length > 0 && halfStories.map((halfStory) => (
-  <li key={halfStory.id}>
-  onClick={() => handleItemClick(halfStory.id)}
-                
-                  {halfStory.story}
-                </li>
-              ))}
-            </ul>
-          </div>
+              <li
+                key={halfStory.id}
+                onClick={() => handleItemClick(halfStory.id)}
+              >
+                {halfStory.story}
+              </li>
+            ))}
+          </ul>
+        </div>
       </section>
-      
     </div>
   );
 }
