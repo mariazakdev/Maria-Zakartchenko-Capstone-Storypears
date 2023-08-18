adcd10ca156029efc9787377b41d1bdb0c8105a3
Maria Zak <mzakart@gmail.com>
2023-08-18 18:44:29 -0400

    Added placeholder avatar nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,24 @@
 import "./Nav.scss";
 import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const NavBar = () => {
+
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    // fetching for nav avatar and name
+    axios.get('http://localhost:8080/users')
+      .then(response => {
+        const user = response.data[0]; // placeholder user
+        setUserData(user);
+      })
+      .catch(error => {
+        console.error('Error fetching user data:', error);
+      });
+  }, []);
+
   return (
     <nav className="nav">
       <ul className="nav-list">
@@ -32,6 +49,19 @@ const NavBar = () => {
         <li>
           <Link to="/story/depot">Depot</Link>
           
+        </li>
+        <li className="nav-list__user">
+          {userData && (
+            <>
+              <div className="user__avatar-circle">
+                <img src={userData.avatar_url || '/default-avatar.png'} alt="User Avatar" />
+              </div>
+              <div className="user__info">
+                <span className="username">{userData.username}</span>
+                <span className="logout">Log out</span>
+              </div>
+            </>
+          )}
         </li>
       </ul>
     </nav>
