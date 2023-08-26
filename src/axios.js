4a62f8bfa9864011adbd889372865b9ee5ea1d52
Maria Zak <mzakart@gmail.com>
2023-08-26 13:55:00 -0400

    Added halfstories to axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+// Define the base URL for your API
 const baseURL = 'http://localhost:8080';
 
+// Create an Axios instance with common settings
 const axiosInstance = axios.create({
   baseURL,
   timeout: 5000,
@@ -65,4 +67,92 @@ const getUserById = async (userId) => {
   }
 };
 
-export { getStoryById, getStories, getUsers, addUser, getUserById };
+// Function to add a new story with an ID
+const addStory = async (storyId, newStory) => {
+  try {
+    const response = await axiosInstance.post(`/stories/${storyId}`, newStory);
+    return response.data;
+  } catch (error) {
+    console.error('Error adding new story:', error);
+    throw error;
+  }
+};
+
+// Function to update a story with an ID
+const updateStoryById = async (storyId, updatedStory) => {
+  try {
+    const response = await axiosInstance.put(`/stories/${storyId}`, updatedStory);
+    return response.data;
+  } catch (error) {
+    console.error('Error updating story:', error);
+    throw error;
+  }
+};
+// Function to fetch half stories
+const getHalfStories = async () => {
+  try {
+    const response = await axiosInstance.get('/halfstories');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching half stories:', error);
+    throw error;
+  }
+};
+
+// Function to fetch a half story by ID
+const getHalfStoryById = async (halfStoryId) => {
+  try {
+    const response = await axiosInstance.get(`/halfstories/${halfStoryId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching half story by ID:', error);
+    throw error;
+  }
+};
+
+// Function to add a new half story
+const addHalfStory = async (newHalfStory) => {
+  try {
+    const response = await axiosInstance.post('/halfstories', newHalfStory);
+    return response.data;
+  } catch (error) {
+    console.error('Error adding new half story:', error);
+    throw error;
+  }
+};
+
+// Function to update a half story by ID
+const updateHalfStoryById = async (halfStoryId, updatedHalfStory) => {
+  try {
+    const response = await axiosInstance.put(`/halfstories/${halfStoryId}`, updatedHalfStory);
+    return response.data;
+  } catch (error) {
+    console.error('Error updating half story:', error);
+    throw error;
+  }
+};
+
+// Function to delete a half story by ID
+const deleteHalfStoryById = async (halfStoryId) => {
+  try {
+    const response = await axiosInstance.delete(`/halfstories/${halfStoryId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error deleting half story:', error);
+    throw error;
+  }
+};
+export {
+  getStoryById,
+  getStories,
+  getUsers,
+  addUser,
+  getUserById,
+  addStory,
+  updateStoryById,
+  getHalfStories,
+  getHalfStoryById,
+  addHalfStory,
+  updateHalfStoryById,
+  deleteHalfStoryById,
+};
