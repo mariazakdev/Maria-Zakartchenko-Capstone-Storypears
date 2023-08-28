d2f0a1f3030ef1d92424cc532ad18ca1a49cda18
Maria Zak <mzakart@gmail.com>
2023-08-27 22:53:54 -0400

    Added change to pen name -profile

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -7,15 +7,24 @@ function MyProfile({ userData }) {
   const [isEditMode, setIsEditMode] = useState(false);
   const [editedBio, setEditedBio] = useState(userData.bio);
   const [editedLinks, setEditedLinks] = useState(userData.links);
+  const [editedPenFirstName, setEditedPenFirstName] = useState(userData.pen_first_name);
+  const [editedPenLastName, setEditedPenLastName] = useState(userData.pen_last_name);
+  
   const bioRef = useRef(null);
+  const penFirstNameRef = useRef(null);
+  const penLastNameRef = useRef(null);
+
   const [bioCharacterCount, setBioCharacterCount] = useState(userData.bio.length); // Initialize with the current bio length
   const toggleButtonValue = () => {
     if (isEditMode) {
       // Handle Update Button Click
       axios
         .put(`http://localhost:8080/users/${userData.id}`, {
+          pen_first_name: editedPenFirstName,
+          pen_last_name: editedPenLastName,
           bio: editedBio,
           links: editedLinks,
+         
         })
         .then((response) => {
           // Handle successful update, e.g., show a success message
@@ -56,14 +65,38 @@ function MyProfile({ userData }) {
           {isEditMode ? "Update" : "Edit"}
         </button>
       </div>
-
+{/* Pen Names section */}
+<div className="profile-info">
+  <h3>Pen Names:</h3>
+  {isEditMode ? (
+    <div>
+      {/* Input fields for editing pen names */}
+      <input
+        type="text"
+        className="profile-info__pen-name-edit"
+        value={editedPenFirstName || ''}
+        onChange={(e) => setEditedPenFirstName(e.target.value)}
+        placeholder="First Name"
+        ref={penFirstNameRef}
+      />
+      <input
+        type="text"
+        className="profile-info__pen-name-edit"
+        value={editedPenLastName || ''}
+        onChange={(e) => setEditedPenLastName(e.target.value)}
+        placeholder="Last Name"
+        ref={penLastNameRef}
+      />
+    </div>
+  ) : (
+    // Display pen names as static text
+    <p className="profile-info__pen-name">
+      {userData.pen_first_name} {userData.pen_last_name}
+    </p>
+  )}
+</div>
       <div className="profile-greeting">
-        <div className="profile-pic">
-          <h2>
-            {userData.pen_first_name} {userData.pen_last_name}
-          </h2>
-          <Avatar className="avatar__image-sm" />
-        </div>
+       
 
         <div className="profile-info">
   <h3>Bio:</h3>
