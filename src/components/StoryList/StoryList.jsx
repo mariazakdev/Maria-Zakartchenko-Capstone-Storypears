8cedd28b250de1b8f8e0a3cfc468a7fb2b47a658
Maria Zak <mzakart@gmail.com>
2023-09-02 23:02:07 -0400

    JS complete for StoryListPage

--- src/components/StoryList/StoryList.jsx
+++ src/components/StoryList/StoryList.jsx
@@ -1,40 +1,112 @@
-import './StoryList.scss';
+import "./StoryList.scss";
 import Avatar from "../Avatar/Avatar";
-import { useNavigate} from 'react-router-dom';
-
-
-
-function StoryList({fullStories}) {
+import { useNavigate } from "react-router-dom";
+import MiniPearTree from "../../assets/icons/1665403462pear-tree.png";
+import GenreFilter from "../GenresFilter/GenresFilter";
+import EmotionsFilter from "../EmotionsFilter/EmotionsFilter";
+import { useState } from "react";
+function StoryList({ fullStories }) {
   const navigate = useNavigate();
 
-  console.log("Rendering StoryList with:", fullStories);
+  const [selectedGenre, setSelectedGenre] = useState("All");
+  const [selectedEmotion, setSelectedEmotion] = useState("All");
+
+  const genres = Array.from(
+    new Set(
+      fullStories.map((story) => {
+        const storiesData =
+          typeof story.stories_data === "string"
+            ? JSON.parse(story.stories_data)
+            : story.stories_data;
+        return storiesData[0].genre;
+      })
+    )
+  );
+
+  const emotions = Array.from(
+    new Set(
+      fullStories.map((story) => {
+        const storiesData =
+          typeof story.stories_data === "string"
+            ? JSON.parse(story.stories_data)
+            : story.stories_data;
+        return storiesData[0].emotion;
+      })
+    )
+  ).filter((emotion) => emotion);
+
   const handleStoryClick = (storyId) => {
-    // handle the click event here, possibly navigate to a story detail page or something else
     console.log("Clicked story with ID:", storyId);
-      navigate(`/stories/${storyId}`);
-    
-  }
+    navigate(`/stories/${storyId}`);
+  };
 
-
-  return (
-    <div className='story'>
-    <h2>The Pear Tree Stories</h2>
-    <ul className="story-list">
-      {fullStories && fullStories.map(storyItem => {
-        const storiesData = typeof storyItem.stories_data === 'string' 
-                            ? JSON.parse(storyItem.stories_data) 
-                            : storyItem.stories_data;
+  const filterStories = () => {
+    return fullStories.filter(story => {
+        const storiesData = typeof story.stories_data === 'string'
+            ? JSON.parse(story.stories_data)
+            : story.stories_data;
         const firstStory = storiesData[0];
 
-        return (
-          <li className="story-list__item" key={storyItem.id} onClick={() => handleStoryClick(storyItem.id)}>
-            <p>{firstStory.title}</p>
-            <Avatar className='avatar__image-sm' />
-          </li>
-        );
-      })}
-    </ul>
-  </div>
+        return (selectedGenre === "All" || firstStory.genre === selectedGenre) &&
+               (selectedEmotion === "All" || firstStory.emotion === selectedEmotion);
+    });
+};
+
+
+  const handleFilterSelect = (type, value) => {
+    if (type === "genre") {
+      setSelectedGenre(value);
+      setSelectedEmotion("All");
+    } else if (type === "emotion") {
+      setSelectedEmotion(value);
+      setSelectedGenre("All");
+    }
+  };
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+  const shuffledStories = shuffleArray(filterStories());
+
+  return (
+    <div className="story">
+      <h2>The Pear Tree Stories</h2>
+      <GenreFilter
+        selectedGenre={selectedGenre}
+        handleGenreSelect={(genre) => handleFilterSelect("genre", genre)}
+        genres={genres}
+      />
+      <EmotionsFilter
+        selectedEmotion={selectedEmotion}
+        handleEmotionSelect={(emotion) =>
+          handleFilterSelect("emotion", emotion)
+        }
+        emotions={emotions}
+      />
+      <ul className="story-list">
+        {shuffledStories.map((storyItem) => {
+          const storiesData =
+            typeof storyItem.stories_data === "string"
+              ? JSON.parse(storyItem.stories_data)
+              : storyItem.stories_data;
+          const firstStory = storiesData[0];
+
+          return (
+            <li
+              className="story-list__item"
+              key={storyItem.id}
+              onClick={() => handleStoryClick(storyItem.id)}
+            >
+              <p>{firstStory.title}</p>
+              <img src={MiniPearTree} alt="tree" />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
