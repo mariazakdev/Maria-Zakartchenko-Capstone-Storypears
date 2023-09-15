d7d740722f1578c42a9030caa6b5cbbaeaa8aada
Maria Zak <mzakart@gmail.com>
2023-09-14 21:03:09 -0400

    Updated homepage description

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,38 +5,37 @@ import { Link } from 'react-router-dom';
 function HomePage() {
   return (
     <>
-    <div className='site'>
-     
-      <section className='site__content'>
-        <div className="intro">
-          <h1>Welcome to StoryPears!</h1>
-          <p >Where your words find their other half <span>or third, or fourth...</span></p>
-          <Link to='/login' className="intro__login-btn">Login to Join the Fun!</Link>
+      <div className='site'>
 
-          <div className="image-container">
-            
-          <img src={Pears} alt="Pair of Pears celebrating creativity" />
-        </div>
-        </div>
+        <section className='site__content'>
+          <div className="intro">
+            <h1>Welcome to StoryPears!</h1>
+            <p>Where stories start with an inspiring seed, branch out with community creativity, and grow into captivating trees. <span>Ready to contribute?</span></p>
+            <Link to='/login' className="intro__login-btn">Login to Begin!</Link>
+
+            <div className="image-container">
+              <img src={Pears} alt="Pair of Pears representing collective creativity" />
+            </div>
+          </div>
+          
+          <div className="features">
+            <p>A <strong>SEED</strong> is a story starter, a hint of inspiration. From a single sentence to a poetic rhythm, it’s the beginning of something wonderful.</p>
+            <p>With your story <strong>SEED</strong> planted, invite others to nurture it. As they contribute, it becomes a <strong>BRANCH</strong>, expanding in exciting, unexpected ways.</p>
+            <p>Once your story has been shaped and contributed to by the community, it stands tall as a complete <strong>TREE</strong>. Every tree in our forest is a testament to collective creativity.</p>
+            <p>Experiment, play around, dive deep, or be whimsical. Let your unique voice shine and have fun collaborating!</p>
+          </div>
+          <div className="mission">
+            <h2>Our Prime Directive:</h2>
+            <p>A story here isn't solitary. It thrives on collaboration. Partner up, or as we like to say... find your pear! Together, let's weave tales that are larger than life.</p>
+          </div>
+          <div className="philosophy">
+            <p>At StoryPears, we champion the potential of every idea. Through collaboration, these seeds of imagination grow into vast forests, where each tree embodies shared creativity. Ready to branch out with us?</p>
+          </div>
         
-        <div className="features">
-          <p>Begin with a story <strong>SEED </strong> seed, let it spark your imagination, and watch a narrative sprout into a <strong>PEAR BRANCH.</strong></p>
-          <p>A prompt , an emotional sentence to inspire a poetic rhythm, or something on your own in our <strong>GARDEN</strong>.</p>
-          <p>Rally your fellow <strong>PEARS.</strong> After all, a story isn't just one-sided. It's multidimensional, just like a <strong> pear tree!</strong></p>
-          <p>Be silly. Be experimental. Be profound. But most importantly, be you and have fun!</p>
-        </div>
-        <div className="mission">
-          <h2>Our Only Rule:</h2>
-          <p>It takes two (or more) to complete a tale. Find your pair, or should we say... pear? And let your combined creativity flow.</p>
-        </div>
-        <div className="philosophy">
-          <p>At StoryPears, we believe in nurturing every seed of an idea. Together, we cultivate forests of imagination where each tree is a testament to collaborative creativity. So, are you ready to branch out?</p>
-        </div>
-      
-      </section>
-      
-    </div>
-     </>
+        </section>
+
+      </div>
+    </>
   );
 }
 
